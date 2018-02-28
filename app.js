348df7bb2644e2212c6f733eb04db26a8819b030
winfan <121552591@qq.com>
2018-02-28 12:29:12 +0800

    static

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const PORT = 3000
 
 const app = express()
+
+app.use('/static', express.static(__dirname + '/static'));
 app.get('/',function(req,res){
     res.send('hello Hello docker world auto build and release success')
 })
